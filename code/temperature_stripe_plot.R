51fc4ac8e2108f405ac4ed02d6143086a3a59617
Arslan Tariq <mywebg9@gmail.com>
2022-05-27 18:37:34 +0500

    Reformatted code

--- code/temperature_stripe_plot.R
+++ code/temperature_stripe_plot.R
@@ -9,23 +9,34 @@ t_data <- read_csv("data/GLB.Ts+dSST.csv", skip = 1, na = "***") %>%
 t_plot <- t_data %>%
   ggplot(aes(x = year, y = 1, fill = t_diff)) +
   geom_tile(show.legend = FALSE) +
-  scale_fill_stepsn(colors = c("#08306B", "white", "#67000D"),
-                    values = rescale(c(min(t_data$t_diff), 0, max(t_data$t_diff))),
-                    n.breaks = 12) +
-  coord_cartesian(expand = FALSE) +
-  scale_x_continuous(breaks = seq(1890,2020, 20)) +
-  labs(
-    title = glue("Global temperature change ({min(t_data$year)}-{max(t_data$year)})")
+  scale_fill_stepsn(
+    colors = c("#08306B", "white", "#67000D"),
+    values = rescale(c(
+      min(t_data$t_diff), 0, max(t_data$t_diff)
+    )),
+    n.breaks = 12
   ) +
+  coord_cartesian(expand = FALSE) +
+  scale_x_continuous(breaks = seq(1890, 2020, 20)) +
+  labs(title = glue(
+    "Global temperature change ({min(t_data$year)}-{max(t_data$year)})"
+  )) +
   theme_void() +
   theme(
-    plot.title = element_text(color = "white",  margin = margin(b = 5, t = 10, unit = "pt"),
-                              hjust = 0.05),
-    axis.text.x =  element_text(color = "white", margin = margin(t = 5, b = 10, unit = "pt")),
+    plot.title = element_text(
+      color = "white",
+      margin = margin(b = 5, t = 10, unit = "pt"),
+      hjust = 0.05
+    ),
+    axis.text.x =  element_text(color = "white", margin = margin(
+      t = 5, b = 10, unit = "pt"
+    )),
     plot.background = element_rect(fill = "black"),
   )
 
-ggsave(plot = t_plot,
-       file = "figures/warmings_strips.png",
-       width = 8,
-       height = 4.5)
+ggsave(
+  plot = t_plot,
+  file = "figures/warmings_strips.png",
+  width = 8,
+  height = 4.5
+)
